bbbfc0dfe4a85c8336cf997c05160c7c13ccd58a
yanxingzhe <yanxingzhe@pinduoduo.com>
2021-04-30 13:20:29 +0800

    fix: try babel(ongoing)

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,4 +1,5 @@
-import { transform } from "@babel/core";
+import { parse, transformFromAst } from "@babel/core";
+// import { parse } from "@babel/parser";
 
 const build = async () => {
   const code = `
@@ -13,20 +14,45 @@ const build = async () => {
     );
   }
   `;
-  transform(
-    code,
-    {
-      // ast: true,
-      filename: "fileEntryPath",
-      presets: ["@babel/preset-typescript"],
-      plugins: ["@babel/plugin-transform-runtime"],
-    },
-    function (_err, result) {
-      console.log("...err", _err);
-      const { code: outputCode } = result || {};
-      console.log("...outputCode", outputCode);
-    }
-  );
+
+  // https://github.com/anyexinglu/taroToWeb/commit/7abdbfc77433e9d8d5dcf4e9e02d32b479110927
+  const result = await parse(code, {
+    // ast: true,
+    sourceType: "unambiguous",
+    filename: "fileEntryPath",
+    // presets: ["@babel/preset-typescript"],
+    // presets: [["@babel/preset-typescript", {}]],
+    plugins: [
+      "typescript",
+      // "jsx",
+      // "exportDefaultFrom",
+      // "typescript",
+      // "exportNamespaceFrom",
+      // "importAssertions",
+    ],
+    // plugins: ["@babel/plugin-transform-runtime"],
+  });
+
+  console.log("...result", result);
+
+  const newCode = await transformFromAst(result);
+
+  console.log("...newCode", newCode);
+
+  // transform(
+  //   code,
+  //   {
+  //     // ast: true,
+  //     filename: "fileEntryPath",
+  //     presets: [["@babel/preset-typescript", {}]],
+  //     plugins: ["@babel/plugin-transform-runtime"],
+  //   },
+  //   function (_err, result) {
+  //     console.log("...err", _err);
+  //     const { code: outputCode } = result || {};
+  //     console.log("...outputCode", outputCode);
+  //   }
+  // );
 };
 
 build();
