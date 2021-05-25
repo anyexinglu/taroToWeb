c2ce30278597fd909d53d68fca3672c3945c19ab
yanxingzhe <yanxingzhe@pinduoduo.com>
2021-05-25 20:36:53 +0800

    init: try renderToString

---
+++ Saber/README.md
@@ -0,0 +1,2 @@
+- renderToString
+- renderToStream
---
+++ Saber/src/core/v1/index.tsx
@@ -0,0 +1,23 @@
+console.log("...inner");
+
+import * as React from "react";
+import { renderToString } from "react-dom/server";
+
+const Document = (props) => {
+  return (
+    <html>
+      <header>header</header>
+      <body>
+        <span>inner</span>
+        {props}
+      </body>
+    </html>
+  );
+};
+
+const Hello = () => <div>hello</div>;
+
+// 1、理解 renderToString
+const result = renderToString(React.createElement(Document, <Hello />));
+
+console.log("...result", result);
